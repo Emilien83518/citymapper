5c3cd489d900ad7c1260272f4c1739451c7745f6
etienne <etienne.hawk@gamil.com>
2026-05-13 16:30:17 +0200

    V2(liste stylé

--- import json.py
+++ import json.py
@@ -4,76 +4,37 @@ with open('lille.json','r') as file:
 #fais un dico de la ligne choisi arret par arret
 def dicostop(L):
     st=[]
+    l=[]
     for station in f['lignes'][L]['stations']:
         st.append(station)
-    ligne1={st[0]:[st[1]]}
-    a=1
+    ligne1={st[0]:[st[1],isconnect(st[0],f)]}
     for u in range(1,len(st)):
         if st[u]==st[-1]:
-            ligne1[st[u]]=[st[u-1]]
+            l=[st[u-1]]
+            l.append(isconnect(st[u],f))
+            ligne1[st[u]]=l
         else:
-            ligne1[st[u]]=[st[u-1],st[u+1]]
+            l=[st[u-1],st[u+1]]
+            l.append(isconnect(st[u],f))
+            ligne1[st[u]]=l
     return ligne1
 
-k=dicostop("R")
-print(k)
 
 def isstop(g,fi):
     for i in fi['lignes']:
             for y in fi['lignes'][i]["stations"]:
-                return i
+                    if y == g:
+                        return i
     return 'stop does not exist'
 
-
-def path(de,lde,l,f):
-    path=[]
-    if isconnect(de,l,f)==de:
-        return path
-    else:
-        path.append(lde[de][1])
-        return path(lde[de][1])
-
 # verifie les correspondance
-def isconnect(acs,wl,f):
-    for i in range(0,len(f["correspondances"])):
-        if acs==f["correspondances"][i]["station"]:
-            for a in range(0,len(f["correspondances"][i]["lignes"])):
-                if wl ==  f["correspondances"][i]["lignes"][a]:
-                    return f["correspondances"][i]["station"]
-                else:
-                    return "the correspondance do not exist"
-    return "this stop don't have any correspondance"
-
-Depart=input("start location")
-arrivée=input("arriving location")
-
-lignedepart=isstop(Depart,f)
-lignearrivée=isstop(arrivée,f)
-listelignedepart= dicostop(lignedepart)
-listelignearrivée=dicostop(lignearrivée)
-p=path(Depart,listelignedepart,lignearrivée,f)
-print(p)
-
-
-
-
-def dfs(G, s):
-    colour = dict()
-    for x in G:
-        colour[x] = 'white'
-    path = [s]
-    colour[s] = 'grey'
-    stack = [s]
-    while stack != []:
-        current = stack[-1]
-        white_neighbours = [v for v in G[current]
-                            if colour[v] == 'white']
-    if white_neighbours != []:
-        v = white_neighbours[0]
-        colour[v] = 'grey'
-        path.append(v)
-        stack.append(v)
-    else:
-        stack.pop()
-        colour[current] = 'black'
-    return path
+def isconnect(acs, f):
+    for item in f.get("correspondances", []):
+        if acs == item["station"]:
+            return item["lignes"]
+    return []
+    
+k=dicostop("R")
+b=isconnect('Gare Lille Flandres',f)
+print(b)
+print(k)
